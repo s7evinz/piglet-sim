34fa53e023eb7705acac33150cf6a33219ec2ce9
Slevin Zhang <s7evinz@gmail.com>
2019-08-17 10:18:59 -0400

    Finish main game play design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+// https://create-react-app.dev/docs/importing-a-component
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import Dice from './Dice/Dice';
+import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <section className="section">
@@ -11,13 +13,76 @@ function App() {
           <h2 className="subtitle">What's the best strategy?</h2>
         </div>
       </section>
-      <footer className="footer">
-        <div className="content has-text-centered">
-        <p>
-          <strong>Made</strong> by <a href="https://slevin.im">Slevin Zhang</a>.
-        </p>
-        </div>
-      </footer>
+      <TabNav />
+      <PlayTab />
+    </div>
+  );
+}
+
+function TabNav(props) {
+  return (
+    <div className="tabs is-centered">
+      <ul>
+        <li className="is-active"><a>Play</a></li>
+        <li><a>Simulate</a></li>
+        <li><a>Explanation</a></li>
+      </ul>
+    </div>
+  );
+}
+
+function PlayTab(props) {
+  return (
+    <div>
+      <GameStatus />
+      <StreakContainer />
+      <PlayActionBar />
+    </div>
+  );
+}
+
+function GameStatus(props) {
+  return (
+    <div className="ps-container ps-space-evenly has-text-centered">
+      <div>
+        <h3 className="ps-dice-addition has-text-primary">+6</h3>
+        <Dice value={0} />
+      </div>
+      <div>
+        <h3 className="is-size-5">Winning</h3>
+        <p className="ps-win-amount">$11</p>
+      </div>
+    </div>
+  );
+}
+
+function StreakContainer(props) {
+  return (
+    <div className="ps-streak-container">
+      <p className="has-text-centered">Streak: 0</p>
+      <div className="ps-sm-dice-container">
+        <span className="ps-small-dice">
+          <Dice value={3} size={24} />
+        </span>
+        <span className="ps-small-dice">
+          <Dice value={3} size={24} />
+        </span>
+        <span className="ps-small-dice">
+          <Dice value={3} size={24} />
+        </span>
+      </div>
+    </div>
+  );
+}
+
+function PlayActionBar(props) {
+  return (
+    <div className="ps-container ps-center ps-action-bar">
+      <div className="buttons ps-buttons-box">
+          <span className="button is-primary">🎲 Roll</span>
+          <span className="button is-warning">💰 Take Home</span>
+          <span className="button is-light">🔄 New</span>
+      </div>
     </div>
   );
 }
--- src/Dice/Dice.js
+++ src/Dice/Dice.js
@@ -1,8 +1,22 @@
 // https://create-react-app.dev/docs/importing-a-component
-import React, { Component } from 'react';
+import React from 'react';
+// https://create-react-app.dev/docs/adding-images-fonts-and-files
+// import { ReactComponent as Dice1 } from './Dice-1.svg';
+import d0 from './Dice-0.svg';
+import d1 from './Dice-1.svg';
+import d2 from './Dice-2-b.svg';
+import d3 from './Dice-3-b.svg';
+import d4 from './Dice-4.svg';
+import d5 from './Dice-5-b.svg';
+import d6 from './Dice-6-b.svg';
 
-class Dice extends Component {
-  render() {
-    return <p>Dice</p>
-  }
+// TODO: shaky animation
+function Dice(props) {
+  const size = props.size || 64;
+  console.log('size:', size);
+  const diceList = [d0, d1, d2, d3, d4, d5, d6];
+  const i = props.value;
+  return <img src={diceList[i]} alt={`Dice ${i}`} style={{ width: size }} />
 }
+
+export default Dice;
